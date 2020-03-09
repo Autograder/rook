706955844f8d640f10aabfbb0a0118359f0f0b02
Simonne <sccontre@ucsd.edu>
2020-03-09 11:52:37 -0700

    front page progress

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,29 +1,58 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
-export default class Login extends React.Component {
-  webName = "autograder.";
-  webTitle = (
-    <div className="App-header">
-        <div className="App-header">{this.webName}</div>
-    </div>
-  );
-  loginBox = (
-    <div className="App-login">
-      <form name="login">
-      Username<input type="text" name="userid"/>
-      <br/>
-      Password<input type="password" name="pswrd"/>
-      <br/>
-      <input type="button" value="Login"/>
-      <input type="reset" value="Cancel"/>
-      </form>
-    </div>
-  );
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    form: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: 300,
+        'position' : 'absolute',
+        'left' : '50%',
+        'margin-left' : '-150px',
+        'padding-bottom' : '100px',
+      },
+    },
+    title : {
+      color: 'white',
+      'font-size': '100px',
+      'text-align' : 'center',
+    },
+    text: {
+      'borderColor' : 'white',
+    }
+  }),
+);
 
-  render() {
-    return <div>
-      {this.webTitle}
-      {this.loginBox}
-    </div>
-  }
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#f8f8f8',
+    },
+  },
+});
+
+export default function Login() {
+  const classes = useStyles();
+    return (
+      <div>
+        <h1 className={classes.title}>autograder.</h1>
+        <body>
+          <p>
+            <ThemeProvider theme={theme}>
+            <form className={classes.form} noValidate autoComplete="off">
+              <TextField id="outlined-basic" className={classes.text} color="primary" label="Email" variant="outlined" />
+            </form>
+            <br/><br/><br/><br/>
+            <form className={classes.form} noValidate autoComplete="off">
+              <TextField id="outlined-basic" className={classes.text} color="primary" label="Password" variant="outlined" />
+            </form>
+            </ThemeProvider>
+          </p>
+        </body>
+      </div>
+    );
 }
