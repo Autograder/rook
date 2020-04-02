32710fceda6f672d8ca660a536fdf8ac894ca9a4
sravyabalasa <sravya.balasa@gmail.com>
2020-04-02 10:01:22 -0700

    Update Instructions.tsx

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Navbar from './Navbar';
+//import OurTheme from '../style/Theme';
+//import Styles from '../style/InstructionsStyle';
 
 export default function Instructions() {
+    //const theme = OurTheme.theme;
+	//const classes = Styles.useStyles();
     return (
         <div>
-            <Navbar/>
             <h1> Instructions </h1>
             <h5> Hello! This is where instructions to sign up for Autograder will live. Should be mostly a static page with a few links/redirects. </h5>
         </div>
