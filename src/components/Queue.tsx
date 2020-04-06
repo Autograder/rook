5e0f78f30f749ac246df71cbbe4aa17ec8141f4c
Sravya Balasa <sravya.balasa@gmail.com>
2020-04-05 18:21:48 -0700

    DUMMY ADD TO QUEUE WORKS

--- src/components/Queue.tsx
+++ src/components/Queue.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , { useState } from 'react';
 import Navbar from './Navbar';
 import Message from './Message';
 import Ticket from './Ticket';
@@ -8,25 +8,30 @@ import MessageIcon from '@material-ui/icons/Message';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';import OurTheme from '../style/Theme';
 import Styles from '../style/QueueStyle';
 import IconButton from '@material-ui/core/IconButton';
-// Adds a new ticket to the queue when "+" is clicked
-function addTicket(myList:any) {
-
-}
 
 export default function Queue() {
     const theme = OurTheme.theme;
     const classes = Styles.useStyles();
-    
-    const ticketList = [
-    <Ticket name="Shaeli Yao" location="B250-6"
-    description="I hate programming"
-    time="09:49:29"
-    date="January 24, 2020"/>,
-    <Ticket name="Tiffany Meng" location="B240-12"
-    description="I have a bad bug"
-    time="10:50:30"
-    date="May 15, 2000"/>,
-    <Ticket name="Anonymous" location="" description="This content is hidden"/> ]
+   
+    const [ticketList, setTicketList] = useState([
+        <Ticket name="Shaeli Yao" location="B250-6"
+        description="I hate programming"
+        time="09:49:29"
+        date="January 24, 2020"/>,
+        <Ticket name="Tiffany Meng" location="B240-12"
+        description="I have a bad bug"
+        time="10:50:30"
+        date="May 15, 2000"/>,
+        <Ticket name="Anonymous" location="" description="This content is hidden"/> ]);
+
+    const setTL = (name:string, date:string, time:string, description:string) => {
+        setTicketList(
+        ticketList.concat([
+        <Ticket name={name} location={name}
+        description={description}
+        time={time}
+        date={date}/>]))
+    }
 
     return (
         <div> 
@@ -38,7 +43,7 @@ export default function Queue() {
 
                     </Grid>
                     <Grid item xs={6}>
-                        <IconButton aria-label="delete" onClick={() => addTicket(ticketList)} className={classes.icon}>
+                        <IconButton aria-label="delete" onClick={() => setTL("name", "date", "time", "description")} className={classes.icon}>
                             <AddCircleOutlineIcon/>
                         </IconButton>
                         {ticketList}                                                                         
