98efa71f8df58af0cf5d259ef244233660adcee7
Sravya Balasa <sravya.balasa@gmail.com>
2021-04-09 14:44:41 -0700

    delete commented out things

--- src/conf.js
+++ src/conf.js
@@ -7,16 +7,10 @@ import axios from 'axios';
 
 // this gets changed for production, but this works for local dev
 const serverURL = 'http://localhost:1337';
-axios.defaults.xsrfHeaderName = "X-CSRF-TOKEN"
-axios.defaults.xsrfCookieName = "csrf-token"
 
 // create the axios object used to interact with the API
 const api = axios.create({
-  baseURL: serverURL,
-  // withCredentials: true,
-  // headers: {
-  //   //'X-XSRF-TOKEN': getCookieValue('XSRF-TOKEN')
-  // }
+  baseURL: serverURLs
 });
 
 export default api;
